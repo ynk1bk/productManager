604a6bbe1d3fbf0b3221736070f549b1d82937e6
test <test@example.ru>
2025-02-18 13:11:09 +0300

    Добавление файла Product.cs и нового класса Product

---
+++ ProductManager/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"Товар: {Name}, Цена: {Price}, Количество: {Quantity}";
+        }
+
+        public bool IsAvailable()
+        {
+            return Quantity > 0;
+        }
+    }
+}
+
